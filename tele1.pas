6b8a8ce5bc457e3110a3a0ac74fd8436b59286ac
elvis055 <ealvarez.0374@unimar.edu.ve>
2023-10-29 20:53:41 -0400

    aca ya avanzamos un poco puesto que agregramos mas variables como tambien se mejoro la parte de las estaciones y los tramos,tambien se le agrego el pedir la cantidad de boletos,la edad e imprimir factura, y por ultimo va empezando el apartado de ver sistemas, coomo ultimo detalle se le agrego color a las letras

--- tele1.pas
+++ tele1.pas
@@ -1,47 +1,53 @@
 (*
-   ahi vamos
+   vamos mejor
 *)
 
 program telefericomister;
-    
+
 uses crt;
 	var
-	nombre,apellido: string;
+	nombre,apellido,tipo_b,cedula,origen,destino,conf,precio_u: string;
 	opcion: char;
-	estacion: integer;
-	cedula: string;
-	i: integer;
+	estacion,i,cantidad_b,tramo,edad: integer;
 	contiene_letras:boolean;
-	tramo:integer;
-	
+        precio_b:real;
 begin
-	writeln('_____________________');
-	writeln('     BIENVENIDO      ');
-	writeln('        AL           ');
-	writeln('     TELEFERICO      ');
-	writeln('        DEL          ');
-	writeln('       MISTER        ');
-	writeln('_____________________');
+textcolor(blue);
+textbackground(white);
+	writeln('------------------------------------');
+	writeln('            BIENVENIDO              ');
+	writeln('                AL                  ');
+	writeln('            TELEFERICO              ');
+	writeln('               DEL                  ');
+	writeln('              MISTER                ');
+	writeln('------------------------------------');
+
 writeln('');
-writeln('que desea hacer?');
-writeln('1. comprar boleto');
-writeln('2. ver el sistema');
-writeln ('3. salir');
+writeln('MENU');
+writeln('');
+writeln('1. COMPRAR BOLETOS');
+writeln('2. VER SISTEMA');
+writeln('3. SALIR');
 readln(opcion);
-	case opcion of 
+
+	case opcion of
 	'1': begin
-	writeln('usted ha escogido comprar boleto, por favor ingrese sus datos para la compra');
-	writeln('ingrese su nombre');
-		readln(nombre);
-	writeln('ingrese su apellido');
-		readln(apellido);
-	begin
+  	        writeln('USTED HA ESCOGIDO "COMPRAR BOLETOS", POR FAVOR INGRESE SUS DATOS PARA LA COMPRA:');
+                writeln('');
+	        write('INGRESE SU NOMBRE: ');
+	                readln(nombre);
+                write('INGRESE SU APELLIDO: ');
+		        readln(apellido);
+                write('INGRESE SU EDAD: ');
+                        readln(edad);
+
+
   repeat
-    writeln('Ingrese su cedula:');
+    write('INGRESE SU CEDULA: ');
     readln(cedula);
-    
+
     contiene_Letras := False;
-    
+
     for i := 1 to length(cedula) do
     begin
       if (cedula[i] in ['a'..'z', 'A'..'Z']) then
@@ -50,15 +56,16 @@ readln(opcion);
         break;
       end;
     end;
-    
+
     if contiene_Letras then
       writeln('La cedula no puede contener letras. Por favor, ingrese nuevamente.');
-      
+      writeln('');
   until not contiene_Letras;
 
-  writeln('Cedula ingresada correctamente.');
+  writeln('DATOS INGRESADOS CORRECTAMENTE.');
   writeln('');
-  writeln('INGRESAR SU ESTACION: ');
+   writeln('INGRESAR SU ESTACION: ');
+  repeat
   writeln('');
   writeln('1. BARINITAS');
   writeln('2. LA MONTANA');
@@ -67,35 +74,107 @@ readln(opcion);
   writeln('5. PICO ESPEJO');
   writeln('');
   readln(estacion);
+
+
   case estacion of
 
-   1:begin
+  1:begin
        writeln('');
        writeln('ELIGA SU TRAMO: ');
        writeln('1. BARINITAS - LA MONTANA');
        writeln('2. BARINITAS - LA AGUADA');
        writeln('3. BARINITAS - LOMA REDONDA');
        writeln('4. BARINITAS - PICO ESPEJO');
+       writeln('');
        readln(tramo);
-   end;
-   2:begin
+       writeln('');
+       origen:='BARINITAS';
+
+              CASE tramo of
+
+        1:begin
+            destino:='LA MONTANA';
+            end;
+
+        2:begin
+            destino:='LA AGUADA';
+            end;
+
+        3:begin
+            destIno:='LOMA REDONDA';
+            end;
+
+        4:begin
+            destino:='PICO ESPEJO';
+            end;
+
+          end;
+       end;
+
+  2:begin
        writeln('');
        writeln('ELIGA SU TRAMO: ');
-       writeln('1. LA MONTANA - BARINITA');
+       writeln('1. LA MONTANA - BARINITAS');
        writeln('2. LA MONTANA - LA AGUADA');
        writeln('3. LA MONTANA - LOMA REDONDA');
        writeln('4. LA MONTANA - PICO ESPEJO');
+       writeln('');
        readln(tramo);
-   end;
-   3:begin
+       writeln('');
+       origen:='LA MONTANA';
+
+         CASE tramo of
+
+        1:begin
+            destino:='BARINITAS';
+            end;
+
+        2:begin
+            destino:='LA AGUADA';
+            end;
+
+        3:begin
+            destIno:='LOMA REDONDA';
+            end;
+
+        4:begin
+            destino:='PICO ESPEJO';
+            end;
+      end;
+  end;
+
+  3:begin
        writeln('');
        writeln('ELIGA SU TRAMO: ');
        writeln('1. LA AGUADA - LA MONTANA');
        writeln('2. LA AGUADA - BARINITA');
        writeln('3. LA AGUADA - LOMA REDONDA');
        writeln('4. LA AGUADA - PICO ESPEJO');
+       writeln('');
        readln(tramo);
-   end;
+       writeln('');
+       origen:='LA AGUADA';
+
+       CASE tramo of
+
+        1:begin
+            destino:='LA MONTANA';
+            end;
+
+        2:begin
+            destino:='BARINITA';
+            end;
+
+        3:begin
+            destIno:='LOMA REDONDA';
+            end;
+
+        4:begin
+            destino:='PICO ESPEJO';
+            end;
+          end;
+       end;
+
    4:begin
        writeln('');
        writeln('ELIGA SU TRAMO: ');
@@ -103,8 +182,32 @@ readln(opcion);
        writeln('2. LOMA REDONDA - LA AGUADA');
        writeln('3. LOMA REDONDA - BARINITAS');
        writeln('4. LOMA REDONDA - PICO ESPEJO');
+       writeln('');
        readln(tramo);
-   end;
+       writeln('');
+       origen:='LOMA REDONDA';
+
+       CASE tramo of
+
+        1:begin
+            destino:='LA MONTANA';
+            end;
+
+        2:begin
+            destino:='LA AGUADA';
+            end;
+
+        3:begin
+            destIno:='BARINITAS';
+            end;
+
+        4:begin
+            destino:='PICO ESPEJO';
+            end;
+        end;
+
+     end;
+
    5:begin
        writeln('');
        writeln('ELIGA SU TRAMO: ');
@@ -112,80 +215,94 @@ readln(opcion);
        writeln('2. PICO ESPEJO - LA AGUADA');
        writeln('3. PICO ESPEJO - LOMA REDONDA');
        writeln('4. PICO ESPEJO - BARINITAS');
+       writeln('');
        readln(tramo);
-   end;
-end;
-end;
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-end;
-end;
-end.
-
+       origen:='PICO ESPEJO';
+
+     CASE tramo of
+
+        1:begin
+            destino:='LA MONTANA';
+            end;
+
+        2:begin
+            destino:='LA AGUADA';
+            end;
+
+        3:begin
+            destIno:='LOMA REDONDA';
+            end;
+
+        4:begin
+            destino:='BARINITAS';
+            end;
+        end;
+       end;
+     end;
+
+
+  writeln('');
+  writeln('Origen: ',origen);
+  writeln('Destino: ',destino);
+  writeln('');
+  writeln('DESEA CAMBIAR SU ESTACION/TRAMO?');
+  write('Y o N: ');
+  readln(conf);
+  until conf='N';
+
+
+
+
+  WRITELN('');
+  write('CANTIDAD DE BOLETOS: ');
+  readln(cantidad_b);
+  WRITELN('');WRITELN('');WRITELN('');WRITELN('');WRITELN('');WRITELN('');
+
+  if edad < 3 then
+     begin
+     precio_b:=cantidad_b*0;
+     precio_u:='$0';
+     tipo_b:='EXONERADO';
+     end
+
+  else if (edad<=12) OR (edad>=60) then
+       begin
+       precio_b:=cantidad_b*(12*2);
+       precio_u:='$12';
+       tipo_b:='TERCERA EDAD/MENORES DE EDAD';
+       end
+
+  else if  (edad>=13) or (edad<=59) then
+       BEGIN
+       precio_b:=cantidad_b*(20*2);
+       precio_u:='$20';
+       tipo_b:='GENERAL';
+       end;
+       writeln('------------------------------------');
+         writeln('              FACTURA               ');
+         writeln('------------------------------------');
+         writeln('NOMBRE: ',nombre);
+         writeln('APELLIDO: ',apellido);
+         writeln('CEDULA: ',cedula);
+         writeln('------------------------------------');
+         writeln('ESTACION/ORIGEN:       ',origen);
+         writeln('TRAMO/DESTINO:         ',destino);
+         writeln('------------------------------------');
+         writeln('TIPO DE BOLETO:        ',tipo_b);
+         writeln('CANTIDAD DE BOLETOS:   ',cantidad_b);
+         writeln('------------------------------------');
+         writeln('PRECIO UNITARIO:       ',precio_u);
+         writeln('PRECIO TOTAL:          ',precio_b:4:2);
+         writeln('------------------------------------');
+         writeln('       GRACIAS POR SU COMPRA        ');
+         writeln('------------------------------------');
+         
+         end;
+        end;
+        case opcion of
+        '2':begin
+        writeln('hasta ahora se han vendido ',cantidad_b); 
+        write('boletos de tipo',tipo_b);
+        end;
+        end;
+        end.
